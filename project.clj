ee533d98cad3ab853e10f972500e30a6de7fb7f0
Thomas Cothran <tmcothran@gmail.com>
2018-07-08 15:03:21 -0400

    Set clojure/clojurescript versions.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject testing-clj-17 "1.1.0-SNAPSHOT"
   :description "Testing CLJ 1.7"
-  :dependencies [#_[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojure "1.8.0"]
                  [clj-time "0.14.2"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]
-                 #_[org.clojure/clojurescript "1.7.170"]
-                 [org.clojure/clojurescript "1.10.238"]]
+                 [org.clojure/clojurescript "1.7.170"]
+                 #_[org.clojure/clojurescript "1.10.238"]]
   :main clj-test.core  ;; This runs just fine, no matter what the Clojure version
   :plugins [[lein-figwheel "0.5.16"]]
   :clean-targets [:target-path "out"]
